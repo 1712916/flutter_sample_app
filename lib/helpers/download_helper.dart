86687f5aecd431b8dff93ff9cd710340074ac7b7
Kiều Phong BB’m Real <vinhnt@sendo.vn>
2025-02-11 13:27:25 +0700

    fix: install android app

--- lib/helpers/download_helper.dart
+++ lib/helpers/download_helper.dart
@@ -1,14 +1,12 @@
 import 'package:collection/collection.dart';
 import 'package:dio/dio.dart';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/services.dart';
-import 'package:image_downloader/image_downloader.dart';
+// import 'package:image_downloader/image_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import '../resources/resources.dart';
-import '../utils/utils.dart';
 import '../widgets/widgets.dart';
 import 'helpers.dart';
 
@@ -28,15 +26,15 @@ class DownloadHelper {
 
   static _downLoadImage(String url) async {
     try {
-      // Saved with this method.
-      //chọn địa chỉ => Mình sẽ set up ở màn setting
-      //sẽ lưu vào một cái folder nào đó
-      var imageId = await ImageDownloader.downloadImage(url, destination: AndroidDestinationType.directoryPictures);
-      /// todo lỗi khi chọn cái địa chỉ mình tự gán vào
-      // var imageId = await ImageDownloader.downloadImage(url, destination: AndroidDestinationType.custom(directory: SettingManager.downloadPath.split('/').last));
-      if (imageId == null) {
-        return;
-      }
+      // // Saved with this method.
+      // //chọn địa chỉ => Mình sẽ set up ở màn setting
+      // //sẽ lưu vào một cái folder nào đó
+      // var imageId = await ImageDownloader.downloadImage(url, destination: AndroidDestinationType.directoryPictures);
+      // /// todo lỗi khi chọn cái địa chỉ mình tự gán vào
+      // // var imageId = await ImageDownloader.downloadImage(url, destination: AndroidDestinationType.custom(directory: SettingManager.downloadPath.split('/').last));
+      // if (imageId == null) {
+      //   return;
+      // }
       Toast.makeText(message: LocaleKeys.saveToPhone.tr());
     } on PlatformException catch (error) {
       Toast.makeText(message: LocaleKeys.haveAnError.tr());
@@ -62,7 +60,7 @@ class DownloadHelper {
       path = null;
       print('Download image: lỗi tải ảnh');
     }
-    
+
     dio.close();
     return path;
   }
